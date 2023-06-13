e8e1e96e3b3a4ee339eb44963726da4a611c7a2f
gaadhebadal <badalahir124@gmail.com>
2023-06-13 22:20:09 +0530

    up to description

---
+++ app/src/main/java/may/internship/Forgot_password_activity.java
@@ -0,0 +1,14 @@
+package may.internship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Forgot_password_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
